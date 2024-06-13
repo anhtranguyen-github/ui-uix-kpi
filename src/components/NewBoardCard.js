9ad6b0805c58ca4228962acb765864413bf63075
unknown <tranguyen.contact@gmail.com>
2024-06-13 15:24:33 +0700

    fix(add-img): add more images

--- src/components/NewBoardCard.js
+++ src/components/NewBoardCard.js
@@ -16,7 +16,15 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import PublicIcon from "@mui/icons-material/Public";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
-import img1 from "../assets/images/img-1.jpg";
+import img1 from "../assets/images/b2.jpg";
+import img2 from "../assets/images/img-2.jpg";
+import img3 from "../assets/images/img-3.jpg";
+import img4 from "../assets/images/img-4.jpg";
+import img5 from "../assets/images/img-5.jpg";
+import img6 from "../assets/images/img-6.jpg";
+import img7 from "../assets/images/img-7.jpg";
+import img8 from "../assets/images/img-8.jpg";
+import img9 from "../assets/images/b1.jpg";
 
 export default function NewBoardCard({
   handleClose,
